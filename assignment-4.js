d8f01420374791cc25dac0f0e9286d2cb7d5970d
Md. Sajedul Islam <sajedulislms@gmail.com>
2023-08-21 22:29:36 +0600

    assignment-4

---
+++ assignment-4.js
@@ -0,0 +1,11 @@
+function multiply(a, b) {
+    if (a === 0 || b === 0) {
+        return 0;
+    }
+    if (b === 1) {
+        return a;
+    }
+    return a + multiply(a, b - 1);
+}
+
+console.log(multiply(3, 4));
